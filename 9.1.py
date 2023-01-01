33475ffd1fa36208d4515c2f8e3242e6d4533843
le thi phuong linh 16a1/0057.uneti.hn <ltplinh.dhkl16a1hn@sv.uneti.edu.vn>
2023-01-01 16:51:47 +0700

    Update 9.1.py

---
+++ 9.1.py
@@ -0,0 +1,9 @@
+def  in_so ( x ):
+      nếu  x > 0 :
+            print ( '# in ra 1' )
+      eif  x < 0 :
+            in ( '# trong ra -1' )
+      khác :
+            in ( '0' )
+x = int ( input ( 'nhập x:' ))
+trong_so ( x )
